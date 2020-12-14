f13dca65f8f9b355b5eb9cbd9d5f7af8cccbe2e5
Anderson Diogo <anderson.diogo@yahoo.com.br>
2020-12-13 22:59:48 -0300

    adicionando projeto

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
+import React, { Fragment, useState, useEffect } from "react";
+import { ThemeProvider } from "styled-components";
+import { lightTheme, darkTheme } from "./theme";
+import GlobalTheme from "./globals";
+import styled from "styled-components";
 import './App.css';
 
 function App() {
+  const [ theme, setTheme ] = useState('light');
+  const toggleTheme = () => {
+    if (theme === "dark") {
+      window.localStorage.setItem("theme", "ligth");
+      setTheme("ligth");
+    } else {
+      window.localStorage.setItem("theme", "dark");
+      setTheme("dark");
+    }
+  };
+
+  useEffect(() => {
+    const localTheme = window.localStorage.getItem("theme");
+    localTheme && setTheme(localTheme);
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <ThemeProvider theme={theme === "dark" ? lightTheme : darkTheme}>
+      <Fragment>
+        <GlobalTheme />
+        <center>
+
+        <form class="container">
+  <div class="container">
+  <img src="carta.png" alt="boletim informativo"></img>
+    <h1>Inscreva-se para o meu boletim informativo</h1>
+   
+
+    <input type="text" placeholder="meu@email.com.br" name="email" required>
+      </input>
+
+    <div class="clearfix">
+      
+      <button type="submit" class="signupbtn">inscrever-se</button>
+      <ButtonChange onClick={toggleTheme}> Mudar Tema</ButtonChange>
+      </div>
+      
     </div>
+    
+</form>
+          
+          </center>
+      </Fragment>
+    </ThemeProvider>
   );
 }
+
+
+const ButtonChange = styled.button`
+  width: 150px;
+  padding: 14px 20px;
+  margin: 20px 0;
+`;
 
 export default App;
---
+++ src/theme.js
@@ -0,0 +1,14 @@
+export const lightTheme = {
+    body: '#FFFFFF',
+    text: '#363537',
+    toggleBorder: '#FFFFFF',
+    background: '#363537',
+    
+}
+export const darkTheme = {
+    body: '#363537',
+    text: '#FFFFFF',
+    toggleBorder: '#6B8096',
+    background: '#999',
+}
+
